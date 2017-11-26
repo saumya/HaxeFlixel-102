9a8994d7752ad2e127a29ff69018f159ab10c089
saumya <2saumyaray@gmail.com>
2017-11-27 00:52:41 +0530

    fonts added

---
+++ README.md
@@ -0,0 +1,16 @@
+Game of Turns
+=================
+
+
+
+#### Assets
+ - Font [PT Mono][1]
+ - Font [KOMIKA HAND][2]
+
+
+
+
+
+
+[1]: https://www.fontsquirrel.com/fonts/pt-mono
+[2]: https://www.fontsquirrel.com/fonts/Komika-Hand
--- source/states/UserHomeState.hx
+++ source/states/UserHomeState.hx
@@ -2,11 +2,27 @@ package states;
 
 import flixel.FlxState;
 
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+
 class UserHomeState extends FlxState 
 {
 	override public function create():Void
 	{
 		trace('UserHomeState:create');
+		//
+		//var myText = new flixel.text.FlxText(0, 0, 0, "Welcome Back", 34);
+		//myText.screenCenter();
+		//var myText = new FlxText(0, 0, 500); // x, y, width
+		var myText = new FlxText(0, 0, 0); // x, y, width
+		myText.text = "Hello World";
+		//myText.setFormat("assets/fonts/PTM55FT.ttf", 40, FlxColor.WHITE, CENTER);
+		//myText.setFormat("assets/fonts/Komika_Hand.ttf", 40, FlxColor.WHITE, CENTER);
+		myText.setFormat("assets/fonts/Komika_Parch.ttf", 40, FlxColor.WHITE, CENTER);
+		//myText.setBorderStyle(OUTLINE, FlxColor.RED, 1);
+		myText.screenCenter();
+		this.add(myText);
+		//
 		super.create();
 	}
 
