d35030f5ad9944bd15ba385905e0b1e0efc4ed45
saumya <2saumyaray@gmail.com>
2017-11-27 01:05:50 +0530

    Fonts and Button are added

--- source/states/UserHomeState.hx
+++ source/states/UserHomeState.hx
@@ -5,6 +5,8 @@ import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
 
+import flixel.ui.FlxButton;
+
 class UserHomeState extends FlxState 
 {
 	override public function create():Void
@@ -15,13 +17,18 @@ class UserHomeState extends FlxState
 		//myText.screenCenter();
 		//var myText = new FlxText(0, 0, 500); // x, y, width
 		var myText = new FlxText(0, 0, 0); // x, y, width
-		myText.text = "Hello World";
+		myText.text = "Welcome Home";
 		//myText.setFormat("assets/fonts/PTM55FT.ttf", 40, FlxColor.WHITE, CENTER);
 		//myText.setFormat("assets/fonts/Komika_Hand.ttf", 40, FlxColor.WHITE, CENTER);
-		myText.setFormat("assets/fonts/Komika_Parch.ttf", 40, FlxColor.WHITE, CENTER);
+		myText.setFormat("assets/fonts/Komika_Parch.ttf", 46, FlxColor.WHITE, CENTER);
 		//myText.setBorderStyle(OUTLINE, FlxColor.RED, 1);
 		myText.screenCenter();
 		this.add(myText);
+		//
+		var myButton = new FlxButton(0, 0, "Label", myCallback);
+		// Custom graphics
+		//myButton.loadGraphic("assets/custom.png");
+		this.add(myButton);
 		//
 		super.create();
 	}
@@ -29,5 +36,9 @@ class UserHomeState extends FlxState
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+	}
+	public function myCallback():Void
+	{
+		trace('onClick');
 	}
 }
