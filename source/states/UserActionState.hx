09705816a6001cf8c017b79e30bf24c2d96882d4
saumya <2saumyaray@gmail.com>
2017-11-30 23:10:01 +0530

    code cleanup

--- source/states/UserActionState.hx
+++ source/states/UserActionState.hx
@@ -6,7 +6,6 @@ import flixel.FlxSprite;
 import flixel.FlxG;
 
 import flixel.ui.FlxButton;
-import flixel.addons.ui.FlxButtonPlus;
 
 class UserActionState extends FlxState 
 {
@@ -24,15 +23,8 @@ class UserActionState extends FlxState
 	private function construct():Void
 	{
 		FlxG.camera.bgColor = 0xFF999999;
-		/*
-		var s1 = new FlxSprite();
-		s1.loadGraphic("assets/images/ui/blueSheet.png",true,190,48);
-		add(s1);
-		*/
 		
-		//var b1:FlxButtonPlus = new FlxButtonPlus(100,100,onSelectBreakfast,"Breakfast");
 		var b1:FlxButton = new FlxButton(100,100,"Breakfast",onSelectBreakfast);
-		//b1.loadButtonGraphic(s1,s1);
 		b1.loadGraphic("assets/images/ui/blueSheet.png",true,190,48);
 		this.add(b1);
 		
